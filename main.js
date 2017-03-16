4982e8e96490de6d8b5b959e80b4d585427de706
CarlosUASLP <jcrangelramirez@hotmail.com>
2017-03-16 14:49:41 -0600

    Blog final.

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-    $('.botones').mouseenter(function() {
-        $('.botones').hide();
+    $('.botones').click(function() {
+        $('#boton1').hide();
+    });
+    $('#boton2').click(function(){
+        $('#boton1').show();
+    });
+
+    $('#explicacion1').mouseover(function(){
+            $('#demostracion1').show();
+    });
+
+    $('#demostracion1').hide();
+    $('#explicacion1').mouseout(function(){
+            $('#demostracion1').hide();
+    });
+
+    $('#mostrar2').click(function(){
+        $('#adentro').show();
+    });
+
+    $('#adentro').click(function(){
+        $('#adentro').fadeOut(5000);
     });
 });
